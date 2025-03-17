abd2498efd097f2b52e8ce33211965069da89d8f
Constanza Pérez <constanza.vp.o@gmail.com>
2025-03-17 08:51:28 -0300

    Desafío amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo() {
+  let nombres = document.getElementById('amigo').value
+  console.log(nombres)
+  if (nombres === "") {
+    alert('Por favor ingresa un nombre')
+  } else {
+    amigos.push(nombres)
+  }
+  console.log(amigos)
+  document.getElementById('amigo').value = ""
+
+  let lista = document.getElementById('listaAmigos')
+  lista.innerHTML = ""
+// Acá inicio el recorrido para poder hacer la lista con los nombres de los amigos
+for (let i = 0; i < amigos.length; i++) {
+  let li = document.createElement('li')
+  li.textContent = amigos[i]
+  lista.appendChild(li)
+}
+}
+
+
+function sortearAmigo () {
+  if (amigos.length === 0) {
+    alert('Por favor inglesa los nombres de tus amigos para sortearlos')
+  } else {
+    let indiceDeAmigo = Math.floor(Math.random()*amigos.length)
+   console.log(indiceDeAmigo)
+
+   let resultado = document.getElementById('resultado')
+    resultado.innerHTML = amigos[indiceDeAmigo]
+  }
+}
